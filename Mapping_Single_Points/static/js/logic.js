54c7730110a279de846f4a835d59c356fc475b5a
merinak1 <merina.kansakar@gmail.com>
2021-11-11 09:41:39 -0800

    Cleaning up folder

--- Mapping_Single_Points/static/js/logic.js
+++ Mapping_Single_Points/static/js/logic.js
@@ -13,6 +13,8 @@ let map = L.map("mapid", {
     zoom: 4
   });
 */
+//  Add a marker to the map for Los Angeles, California.
+// let marker = L.marker([34.0522, -118.2437]).addTo(map);
 
 // We create the tile layer that will be the background of our map.
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
--- js/config.js
+++
@@ -1,2 +0,0 @@
-//API_KEY 
-const API_KEY ="pk.eyJ1IjoibWVyaW5hayIsImEiOiJja3ZzdzRqamQwMGpyMzBvNmhsa2ZveXRoIn0.scSCcKNe0Uhbo8S8IoFwBA";
